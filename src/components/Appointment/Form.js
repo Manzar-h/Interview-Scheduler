0d1517c9fb7686e8d417ec3e7dcea92c590ac364
Manzar <manzar.hussain@telus.com>
2023-01-13 22:36:13 -0500

    Bugs fixed for <Form> while adding props and state

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,16 @@ const Form = (props) => {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+    }
+    
+    const cancel = () => {
+    reset();
+    props.onCancel();
+    }
+
   return (
       <main className="appointment__card appointment__card--create">
         <section className="appointment__card-left">
@@ -27,7 +37,7 @@ const Form = (props) => {
         </section>
         <section className="appointment__card-right">
           <section className="appointment__actions">
-            <Button danger onClick={props.onCancel}>Cancel</Button>
+            <Button danger onClick={cancel}>Cancel</Button>
             <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
           </section>
         </section>
